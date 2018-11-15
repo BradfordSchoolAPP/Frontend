bbeacdec541c36f2613853d77d57f226700b67d6
Unknown <natalia.guzman.s@usach.cl>
2018-11-15 08:46:51 -0300

    diseño login

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,15 +6,16 @@ import {
     TouchableWithoutFeedback,
     TouchableHighlight,
     Dimensions,
-    ImageBackground
+    ImageBackground,
+    TextInput
 } from 'react-native'
 
 import { LinearGradient } from 'expo';
 
-
 const {width,height} = Dimensions.get('window')
 
 import AppNavigator from '../navigation/AppNavigator'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 /*
 <TouchableHighlight 
@@ -34,11 +35,8 @@ export default class HomePage extends Component{
 
     render(){
         return(
-            <View style={styles.container}> 
-                <View>
-
-                    <ImageBackground
-                    style={styles.image}
+                <ImageBackground
+                    style={styles.container}
                     blurRadius={1.5} 
                     source={require('../images/fondo1.jpg')}
                     >
@@ -52,9 +50,33 @@ export default class HomePage extends Component{
                             top: 0,
                             height:height,
                         }}
-                        />
-
-                        <View style={styles.botones}>
+                    />
+                    <View>
+                        <Text style={styles.title}>Bienvenidos</Text>
+                        <Text style={styles.subtitle}>Centro de Padres Colegio Bradford</Text>
+                    </View>
+                    <View style={styles.form}>
+                        <Icon name="user" size={26} color="white" style={styles.inputIcon}/>
+                        <TextInput
+                                style={styles.input}
+                                placeholder = {'Correo electronico'}
+                                placeholderTextColor = {'rgba(250,250,250,0.7)'}
+                                underlineColorAndroid = {'transparent'}
+                                keyboardType = 'email-address'
+                            /> 
+                    </View>
+                    <View style={styles.form}>
+                        <Icon name="lock" size={26} color="white" style={styles.inputIcon}/>
+                        <TextInput
+                                style={styles.input}
+                                placeholder = {'Contraseña'}
+                                placeholderTextColor = {'rgba(250,250,250,0.7)'}
+                                underlineColorAndroid = {'transparent'}
+                                secureTextEntry ={true}
+                                keyboardType = 'visible-password'
+                            /> 
+                    </View>
+                    <View style={styles.botones}>
                         <TouchableHighlight 
                             style={styles.login}
                             onPress={() => this.props.navigation.navigate('login')}>
@@ -62,58 +84,93 @@ export default class HomePage extends Component{
                             <Text style={styles.text}> Ingresar </Text>
                         </TouchableHighlight>
 
-                        <TouchableHighlight style={styles.login}>
+                        <TouchableHighlight style={styles.registro}>
                             <Text style={styles.text}> Registro </Text>
-                        </TouchableHighlight>
-                        
+                        </TouchableHighlight> 
                     </View>
-
-                    </ImageBackground>
-
-
-                </View>
-
-            
-            </View>
-
-            
+                </ImageBackground>     
         )
     }
 }
 
 const styles = StyleSheet.create({
 
-
     container: {
         flex: 1,
+        alignItems: "stretch",
+        padding: 30
     },
-    image:{
-        width:width,
-        height:height,
-
+    form: {
+        alignItems: "stretch",
+        padding:15,
+        marginBottom:-10,
     },
-    botones:{
-        marginVertical:height/2 + 100,
-        flexDirection: 'column',
-        justifyContent: 'space-between',
+    title: {
+        color:'#298359',
+        textShadowColor:'#171717',
+        textShadowOffset: {width: 0.5, height: 0.5},
+        textShadowRadius:0.5,
+        fontSize:52,
+        textAlign:"center",
+        fontWeight:"bold",
+        marginTop:height/6
     },
-
-    login:{
-        borderWidth:2,
-        borderColor:"white",
-        alignItems:'center',
-        alignSelf:'center',
-        justifyContent:'center',
-        width:200,
-        height:50,
-        borderBottomColor:"white",
-        backgroundColor:"transparent",
-        borderRadius:20,
-        marginVertical:5,
+    subtitle: {
+        color:"white",
+        fontSize:30,
+        textShadowColor:'#171717',
+        textShadowOffset: {width: 2, height: 2},
+        textShadowRadius:3,
+        textAlign:"center",
+        marginBottom:25
+    },
+    input:{
+        width:width*0.75,
+        height:40,
+        fontSize:18,
+        marginBottom: 10,
+        paddingLeft: 44,
+        borderBottomColor: 'white',
+        borderBottomWidth: 1,
+        color:'rgba(250,250,250,0.7)'
+    },
+    inputIcon:{
+        position:'absolute',
+        top:20,
+        left:30
     },
     text:{
         color:"white",
         fontSize:18
+    },
+    login:{
+        alignItems:'center',
+        alignSelf:'center',
+        justifyContent:'center',
+        width:width*0.75,
+        height:40,
+        backgroundColor:"#3CA53D",
+        opacity: 10,
+        borderRadius:15,
+        marginVertical:5,
+    },
+    registro:{
+        alignItems:'center',
+        alignSelf:'center',
+        justifyContent:'center',
+        width:width*0.75,
+        height:40,
+        borderWidth:1,
+        borderColor:"white",
+        backgroundColor:"transparent",
+        borderRadius:15,
+        marginVertical:5,
+    },
+    botones:{
+        marginTop:30,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: "stretch",
     }
 })
 
