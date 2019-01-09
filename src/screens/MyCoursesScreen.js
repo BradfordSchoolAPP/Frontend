00f28a70295705dcceebf4e505610f9451f2056e
Esterlina <natalia.guzman.s@usach.cl>
2019-01-09 17:16:13 -0300

    vista de cursos

--- src/screens/MyCoursesScreen.js
+++ src/screens/MyCoursesScreen.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text,TouchableHighlight, View, Button,Dimensions, ScrollView,Image, Picker} from 'react-native';
+import { StyleSheet, Text,TouchableHighlight, View, Button,Dimensions, ScrollView,Image, ImageBackground, Picker} from 'react-native';
 
 import Icon from 'react-native-vector-icons/FontAwesome'
+import Modal from "react-native-modal";
 
 import Header from '../components/Header'
-const {width,height} = Dimensions.get('window');
 import { Dropdown } from 'react-native-material-dropdown';
+import UserAvatar from 'react-native-user-avatar';
+
+const {width,height} = Dimensions.get('window');
+
 
 
 export default class MyCoursesScreen extends React.Component {
@@ -31,16 +35,17 @@ export default class MyCoursesScreen extends React.Component {
         childrens:[],
         namesChildrens: [],
         schoolmates:[],
+        schoolmate:"",
+        parents:[],
         json:[], 
     };
 }
 onChangeText(text) {
   this.setState({ student: text });
-  console.log("el nuevo valor es: "+ this.state.student)
   this.state.childrens.map((item) => {
     if(item.name === text){
       this.setState({ course: item.course });
-      console.log("el item nname es " + item.course.year_course)
+      this.getSchoolmates(item.course.id);
     }
    
   });
@@ -48,6 +53,7 @@ onChangeText(text) {
 updateRef(name, ref) {
   this[name] = ref;
 }
+
 
 getSchoolmates(id){
   return fetch('http://68.183.139.254/api/v1/courses/'+id+'/students')
@@ -62,7 +68,20 @@ getSchoolmates(id){
       console.log(error)
     });
   }
+getParents(id,name){
+  return fetch('http://68.183.139.254/api/v1/students/'+id+'/parents')
+    .then( (response) => response.json() )
+    .then( (responseJson ) => {
+      this.setState({
+        parents: responseJson,
+        schoolmate: name,
+      })
+    })
 
+    .catch((error) => {
+      console.log(error)
+    });
+}
 
 componentDidMount() {
     return fetch('http://68.183.139.254/api/v1/parents/6/students')
@@ -105,13 +124,7 @@ componentDidMount() {
     let student = this.state;
     let course = this.state
     let schoolmates = this.state;
-    console.log("primervalor" +this.state.student)
-    console.log(this.state.childrens)
-    console.log("···········")
-    console.log(this.state.course)
-    console.log("···········KYA")
-    console.log(this.state.schoolmates)
-    console.log("nandeto")
+    let parents = this.state;
     return(
         <View style={styles.container}>
           <Header {...this.props} namePage="Mis cursos"/> 
@@ -141,25 +154,61 @@ componentDidMount() {
                   return (
                       <TouchableHighlight underlayColor="transparent" key={item.name}
                             style={styles.containerSchoolmates} 
-                            onPress={() => console.log("pollito")}
+                            onPress={() => {this.getParents(item.id,item.name); this.setState({modalVisible: true})}}
                           >
                           <View style={{flexDirection: 'row',alignContent:'space-between'}}>
-                          <View style={{backgroundColor:"red",width:60}}>
-
+                          <View style={{width:60, alignContent:"center",alignItems:"center",justifyContent:"center"}}>
+                            <UserAvatar size="55" name={item.name} />
                           </View>
                           <Text style={{fontSize:24, color: "#042e60",marginHorizontal:20,marginVertical:17}}>{item.name}</Text> 
-
                           </View>
                       </TouchableHighlight>                   
                   )
               })}
             </ScrollView>
+            <Modal isVisible={this.state.modalVisible}
+              style={styles.modalContainer}>
+              <View style={styles.containerImage}>
+                <Image
+                style={{tintColor:"white",backgroundColor:'#29a184',width:width*0.75,height:height*0.28}}
+                source={require('../images/Parents-icon.png')}
+                />
+              </View>
+              <View style={{paddingHorizontal:width*0.05, paddingVertical:width*0.05}}>
+                <Text style={{fontSize:18, color:"#042e60",marginBottom:10}}>Apoderado(s) de {this.state.schoolmate}</Text>
+                <ScrollView style={{height:height*0.15}}>
+                {this.state.parents.map((item)=> {
+                  return(
+                    <View>
+                    <View style={{marginVertical:5, borderBottomColor: '#bbb',borderBottomWidth: StyleSheet.hairlineWidth,}}>
+                      <Icon name="user" size={22} color="gray" style={{position:"absolute", marginBottom:12}}/>
+                      <Text style={{fontSize:18,color:"gray", marginLeft: 30}}>{item.name}</Text>
+                    </View> 
+                    <View style={{marginVertical:5, borderBottomColor: '#bbb',borderBottomWidth: StyleSheet.hairlineWidth,}}>
+                    <Icon name="envelope-o" size={20} color="gray" style={{position:"absolute", marginBottom:12}}/>
+                    <Text style={{fontSize:18,color:"gray", marginLeft: 30}}>{item.email}</Text>
+                    </View> 
+                    <View style={{marginVertical:5, borderBottomColor: '#bbb',borderBottomWidth: StyleSheet.hairlineWidth,}}>
+                      <Icon name="phone" size={22} color="gray" style={{position:"absolute", marginBottom:12}}/>
+                      <Text style={{fontSize:18,color:"gray", marginLeft: 30}}>{item.phone_number}</Text>
+                    </View>
+                    </View>
+                  )
+                })}
+                </ScrollView>
+                <View style={styles.button}>
+                    <TouchableHighlight style={styles.button} onPress={() => this.setState({modalVisible:false})}>
+                        <Text style={{fontSize:20,color:"white"}}>Hecho</Text>
+                    </TouchableHighlight>
+                </View>
+              </View>  
+                    
+            </Modal>
           </View>
+         
         </View>
     );
   }
-      
-
 }
 
 const styles = StyleSheet.create({
@@ -171,15 +220,46 @@ const styles = StyleSheet.create({
     marginVertical: height*0.04,
     marginHorizontal: width*0.1
   },
+  modalContainer: {
+    backgroundColor:"white",
+    borderRadius:10,
+    marginTop: height* 0.2,
+    marginBottom: height*0.13,
+    marginHorizontal: width* 0.1,
+    width: width* 0.8,
+    alignItems: undefined,
+        justifyContent: undefined,
+  },
+  containerImage:{
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    backgroundColor:'#29a184',
+    height:height*0.3,
+    paddingVertical: height* 0.02,
+    width:width*0.8,alignItems: "center"
+  },
   icon: {
     position:'absolute',
    },
    containerSchoolmates:{
     height:height*0.1,
-
     borderTopColor:'#ACABAB',
     borderTopWidth:1,
     borderBottomColor:'#ACABAB',
     borderBottomWidth:1
+  },
+  button: {
+    backgroundColor: "#29a184",
+    marginHorizontal:5,
+    marginVertical:20,
+    width: width*0.6,
+    height: 40,
+    borderColor: "transparent",
+    borderWidth: 0,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignContent:"center",
+    alignItems:"center",
+    alignSelf:"center"
   },
 })
